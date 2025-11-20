e219bebf2d962337431c13344c3fd9c3192bd7a7
Chanchal Aggarwal <165173154+CHANCHALAGGARWAL@users.noreply.github.com>
2025-11-20 20:25:57 +0530

    Add files via upload

---
+++ SumofDigit.java
@@ -0,0 +1,12 @@
+public class SumofDigit {
+    public static void main(String[] args){
+        int number = 123;
+        int sum = 0;
+        while(number > 0){
+            int digit = number % 10;
+            sum = sum + digit;
+            number = number / 10;
+        }
+        System.out.println("Sum of digits is : " + sum);
+    }
+}
